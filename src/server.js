5c7199ff22dec0cdddb60921ca7313fe6bb3c164
kieupie <92916187+kieupie@users.noreply.github.com>
2024-01-23 21:19:53 +0700

    use mysql2

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const configViewEngine = require('./config/viewEngine');
 const webRouter = require('./routes/web');
+const mysql = require('mysql2')
 
 const app = express();
 const hostname = process.env.PORT || 8081;
@@ -13,6 +14,24 @@ configViewEngine(app);
 
 //router routes
 app.use('/v1', webRouter);
+
+//test connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root', //default: empty string
+    password: '12345678a',
+    database: 'learnbe'
+});
+
+//simple query
+connection.query(
+    'SELECT * FROM Users',
+    function (err, results, fields) {
+        console.log('results', results)
+        console.log('fields', fields)
+    }
+)
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
